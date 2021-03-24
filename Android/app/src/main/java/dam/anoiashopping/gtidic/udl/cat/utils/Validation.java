20e9a502def941029760eeac221af258ffd42d5f
soc-un-zombi <joan.f.pedro@protonmail.com>
2021-03-24 22:59:40 +0100

    Arreglat error Gradle

--- Android/app/src/main/java/dam/anoiashopping/gtidic/udl/cat/utils/Validation.java
+++ Android/app/src/main/java/dam/anoiashopping/gtidic/udl/cat/utils/Validation.java
@@ -1,10 +1,29 @@
-package dam.anoiashopping.gtidic.udl.cat.utils;
+/*package dam.anoiashopping.gtidic.udl.cat.utils;
+
+import dam.anoiashopping.gtidic.udl.cat.R;
 
 public class Validation {
 
     //TODO
 
-    public  isValidUsername () {
+    public static ValidationResultImpl isValidUsername (String username) {
+
+        String msg = "";
+        boolean isValid = true;
+
+        getUsernameError (msg, isValid, username);
+
+        return new ValidationResultImpl(msg, isValid);
+    }
+
+    public static void getUsernameError (String msg, boolean isValid, String username) {
+
+        if (username.isEmpty()) {
+            msg = getString(R.string.emptyUsername);
+            isValid = false;
+            return;
+        }
+
 
     }
-}
+}*/
