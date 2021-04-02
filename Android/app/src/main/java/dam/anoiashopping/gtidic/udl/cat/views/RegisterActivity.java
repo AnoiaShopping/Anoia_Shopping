10bf644b251a2221ec9211913ed5ecc9f16e3d58
Jordi Mateo <jordi.mateo@udl.cat>
2021-04-02 08:06:03 +0200

    Fix problemes ActivityRegister and ViewModel with observer

--- Android/app/src/main/java/dam/anoiashopping/gtidic/udl/cat/views/RegisterActivity.java
+++ Android/app/src/main/java/dam/anoiashopping/gtidic/udl/cat/views/RegisterActivity.java
@@ -8,11 +8,13 @@ import android.widget.CompoundButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import dam.anoiashopping.gtidic.udl.cat.R;
 import dam.anoiashopping.gtidic.udl.cat.databinding.ActivityRegisterBinding;
 import dam.anoiashopping.gtidic.udl.cat.utils.EULA;
+import dam.anoiashopping.gtidic.udl.cat.utils.ValidationResultImpl;
 import dam.anoiashopping.gtidic.udl.cat.viewmodels.SignUpViewModel;
 
   public class RegisterActivity extends AppCompatActivity {
@@ -30,6 +32,15 @@ import dam.anoiashopping.gtidic.udl.cat.viewmodels.SignUpViewModel;
 
     private void initView () {
 
+        SignUpViewModel signUpViewModel = new ViewModelProvider(this).get(SignUpViewModel.class);
+        ActivityRegisterBinding activityRegisterBinding = DataBindingUtil.setContentView(this, R.layout.activity_register);
+        activityRegisterBinding.setLifecycleOwner(this);
+        activityRegisterBinding.setViewModel (signUpViewModel);
+
+        // Fixa't que observes les modificacions:
+        signUpViewModel.FirstNameValidator.observe(this,
+                validationResultI -> activityRegisterBinding.iFirstName.setError(getString(validationResultI.getMsgError())));
+
         Accept_Terms_conditions = findViewById (R.id.c_AcceptConditions);
         Accept_Terms_conditions.setOnCheckedChangeListener(
                 new CompoundButton.OnCheckedChangeListener() {
@@ -39,17 +50,15 @@ import dam.anoiashopping.gtidic.udl.cat.viewmodels.SignUpViewModel;
             }
         });
 
-        register = findViewById (R.id.b_registrarse);
-        register.setOnClickListener(new View.OnClickListener() {
+        //register = findViewById (R.id.b_registrarse);
+        /*register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //TODO : Aqui posava el que m'havies posat al correu a la part del activity
+                // No cal les modificacions les fa el viewModel.
             }
-        });
+        });*/
 
-        SignUpViewModel signUpViewModel = new ViewModelProvider(this).get(SignUpViewModel.class);
-        ActivityRegisterBinding activityRegisterBinding = DataBindingUtil.setContentView(this, R.layout.activity_register);
-        activityRegisterBinding.setLifecycleOwner(this);
-        activityRegisterBinding.setViewModel (signUpViewModel);
+
     }
 }
