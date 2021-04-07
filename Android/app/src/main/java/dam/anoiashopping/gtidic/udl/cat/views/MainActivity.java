31c7209af86db40eb6d8cbf89ee3916ec450fc81
Joan Francesc Pedro Garcia <SocUnZombi@localhost.localdomain>
2021-04-07 16:42:09 +0200

    Main Activity added

---
+++ Android/app/src/main/java/dam/anoiashopping/gtidic/udl/cat/views/MainActivity.java
@@ -0,0 +1,16 @@
+package dam.anoiashopping.gtidic.udl.cat.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import dam.anoiashopping.gtidic.udl.cat.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
