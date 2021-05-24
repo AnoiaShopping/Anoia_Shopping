215d438cd65135550f796636d49b3b6dab30cb94
Abel Esteban <abelestebanferrer4gmail.com>
2021-05-24 15:15:59 +0200

    Joan fes prova amb vpn regex registre botiga

--- Android/app/src/main/java/dam/anoiashopping/gtidic/udl/cat/views/CreateBusinessActivity.java
+++ Android/app/src/main/java/dam/anoiashopping/gtidic/udl/cat/views/CreateBusinessActivity.java
@@ -58,38 +58,87 @@ public class CreateBusinessActivity extends AppCompatActivity {
 
         btCrearBotiga.setOnClickListener(v -> {
             //implementar regex
+            String provadorRegex = String.valueOf(txtEditNom.getText());
+
             int validadorRegex = 0;
 
-            while(validadorRegex !=5){
-                String provadorRegex = String.valueOf(txtEditNom.getText());
-                /*if (!provadorRegex.matches("^{2,15}[a-zA-Z]+\\.?")){
-                    txtEditNom.setError("Nom no vàlid, ha d'incoure    ");
-                }*/
-                provadorRegex = String.valueOf(txtEditDefinicio.getText()); //qualsevol lletra, espais i enters permesos.
-                if (!provadorRegex.matches("^(.|\\s)*[a-zA-Z]+(.|\\s)*$")){
-                    txtEditDefinicio.setError("Definició no valida. Ha d'incloure   ");
-                }else{validadorRegex++;}
-                provadorRegex = String.valueOf(txtEditInstagram.getText());  //https://www.instagram.com/username
-                if (!provadorRegex.matches("/(?:(?:http|https):\\/\\/)?(?:www\\.)?(?:instagram\\.com|instagr\\.am)\\/([A-Za-z0-9-_\\.]+)/im")){
-                    txtEditInstagram.setError("Instagram incorrecte, has d'incloure el link que redirecciona al perfil");
-                }else{validadorRegex++;}
-                provadorRegex = String.valueOf(txtEditTwitter.getText()); // http://twitter.com/barackobama
-                if (!provadorRegex.matches("/^(?:https|http:\\/\\/)?(?:www\\.)?twitter\\.com\\/(#!\\/)?[a-zA-Z0-9_]+$/i")){
-                    txtEditTwitter.setError("Instagram incorrecte, has d'incloure el link que redirecciona al perfil");
-                }else{validadorRegex++;}
-                provadorRegex = String.valueOf(txtEditWeb.getText()); //Inici amb http o https
-                if (!provadorRegex.matches("^https?:\\/\\/(.*)")){
-                    txtEditWeb.setError("El teu link ha d'incloure un inici https");
+            if (!provadorRegex.matches("^{2,15}[a-zA-Z]+\\.?")){
 
-                }else{validadorRegex++;}
-                provadorRegex = String.valueOf(txtEditFacebook.getText());  //http://www.facebook.com/someusername
-                if (!provadorRegex.matches("(?:http|https:\\/\\/)?(?:www.)?facebook.com\\/(?:(?:\\w)*#!\\/)?(?:pages\\/)?(?:[?\\w\\-]*\\/)?(?:profile.php\\?id=(\\d.*))?([\\w\\-]*)?")){
-                    txtEditFacebook.setError("Instagram incorrecte, has d'incloure el link que redirecciona al perfil");
-                }else{validadorRegex++;}
-                
-                
+                txtEditNom.setError("Nom no vàlid, ha d'incoure    ");
+
             }
 
+            provadorRegex = String.valueOf(txtEditDefinicio.getText()); //qualsevol lletra, espais i enters permesos.
+
+            if (!provadorRegex.matches("^(.|\\s)*[a-zA-Z]+(.|\\s)*$")){
+
+                txtEditDefinicio.setError("Definició no valida. Ha d'incloure   ");
+
+            }else{validadorRegex++;}
+
+            provadorRegex = String.valueOf(txtEditInstagram.getText());  //https://www.instagram.com/username
+
+            if (!provadorRegex.matches("/(?:(?:http|https):\\/\\/)?(?:www\\.)?(?:instagram\\.com|instagr\\.am)\\/([A-Za-z0-9-_\\.]+)/im")){
+
+                txtEditInstagram.setError("Instagram incorrecte, has d'incloure el link que redirecciona al perfil");
+
+            }else{validadorRegex++;}
+
+            provadorRegex = String.valueOf(txtEditTwitter.getText()); // http://twitter.com/barackobama
+
+            if (!provadorRegex.matches("/^(?:https|http:\\/\\/)?(?:www\\.)?twitter\\.com\\/(#!\\/)?[a-zA-Z0-9_]+$/i")){
+
+                txtEditTwitter.setError("Instagram incorrecte, has d'incloure el link que redirecciona al perfil");
+
+            }else{validadorRegex++;}
+
+            provadorRegex = String.valueOf(txtEditWeb.getText()); //Inici amb http o https
+
+            if (!provadorRegex.matches("^https?:\\/\\/(.*)")){
+
+                txtEditWeb.setError("El teu link ha d'incloure un inici https");
+
+
+
+            }else{validadorRegex++;}
+
+            provadorRegex = String.valueOf(txtEditFacebook.getText());  //http://www.facebook.com/someusername
+
+            if (!provadorRegex.matches("(?:http|https:\\/\\/)?(?:www.)?facebook.com\\/(?:(?:\\w)*#!\\/)?(?:pages\\/)?(?:[?\\w\\-]*\\/)?(?:profile.php\\?id=(\\d.*))?([\\w\\-]*)?")){
+
+                txtEditFacebook.setError("Instagram incorrecte, has d'incloure el link que redirecciona al perfil");
+
+            }else{validadorRegex++;}
+
+
+
+            if(validadorRegex ==5){
+
+
+
+                Business business = new Business();
+
+                business.setNom(txtEditNom.getText().toString());
+
+                business.setDefinicio(txtEditDefinicio.getText().toString());
+
+                business.setTipus(agafarTipusNegoci());
+
+
+
+                businessViewModel.createBusiness(business);
+
+
+
+                Context context = getApplicationContext();
+
+                CharSequence text = "BOTIGA REGISTRADA";
+
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast.makeText(context, text, duration).show();
+
+            }
             Business business = new Business();
             business.setNom(txtEditNom.getText().toString());
             business.setDefinicio(txtEditDefinicio.getText().toString());
