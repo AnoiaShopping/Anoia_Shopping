2400886bee9b14a5b68e60372f59847f8411e20a
Joan Francesc <Pedro Garcia>
2021-06-02 18:04:04 +0200

    acabar implementar foto

--- Android/app/src/main/java/dam/anoiashopping/gtidic/udl/cat/views/CreateBusinessActivity.java
+++ Android/app/src/main/java/dam/anoiashopping/gtidic/udl/cat/views/CreateBusinessActivity.java
@@ -1,16 +1,27 @@
 package dam.anoiashopping.gtidic.udl.cat.views;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.io.File;
 
 import dam.anoiashopping.gtidic.udl.cat.R;
 import dam.anoiashopping.gtidic.udl.cat.manager.PermissionManager;
@@ -24,6 +35,8 @@ public class CreateBusinessActivity extends AppCompatActivity {
     PermissionManager permissionManager;
     private final int REQUEST_EXTERNAL_STORAGE = 13;
     private final int PICK_IMAGE_REQUEST = 14;
+    ImageView photopreview;
+    File photo;
 
     private EditText txtEditNom;
     private EditText txtEditWeb;
@@ -33,7 +46,6 @@ public class CreateBusinessActivity extends AppCompatActivity {
     private EditText txtEditTwitter;
     private Button btCrearBotiga;
     private CreateBusinessViewModel createBusinessViewModel;
-
 
     @SuppressLint({"Range", "WrongConstant"})
     @Override
@@ -47,7 +59,8 @@ public class CreateBusinessActivity extends AppCompatActivity {
         txtEditInstagram = findViewById(R.id.txtEditInstagram);
         txtEditFacebook  = findViewById(R.id.txtEditFacebook);
         txtEditTwitter = findViewById(R.id.txtEditTwitter);
-        Button btCrearBotiga = findViewById(R.id.btCrearBotiga);
+        btCrearBotiga = findViewById(R.id.btCrearBotiga);
+        photopreview = findViewById(R.id.im_businessphotopreview);
 
         createBusinessViewModel = new CreateBusinessViewModel();
 
@@ -62,101 +75,110 @@ public class CreateBusinessActivity extends AppCompatActivity {
         spinner.setAdapter(adapter);
 
         btCrearBotiga.setOnClickListener(v -> {
-            //implementar regex
-            String provadorRegex = String.valueOf(txtEditNom.getText());
-
-            boolean validadorRegex = true;
-
-            if (!provadorRegex.matches("^{2,20}(.|\\s)*[a-zA-Z]+(.|\\s)*$")){
-
-                txtEditNom.setError("Nom no vàlid, ha d'incoure lletres nomes i un tamany de màxim 25 caràcters i mínim 2");
-                validadorRegex = false;
-
-            }
-
-            provadorRegex = String.valueOf(txtEditDefinicio.getText()); //qualsevol lletra, espais i enters permesos.
-            if (!provadorRegex.matches("^(.|\\s)*[a-zA-Z]+(.|\\s)*$")){
-
-                txtEditDefinicio.setError("Definició no valida. Ha d'incloure caracters");
-                validadorRegex = false;
-
-            }
-
-            provadorRegex = String.valueOf(txtEditWeb.getText()); //Inici amb http o https
-            if (!provadorRegex.matches("^https?:\\/\\/(.*)")){
-                if(!provadorRegex.isEmpty()){
-                    validadorRegex = false;
-                    txtEditWeb.setError("Si no teniu web deixeu buit el camp, si en teniu, utilitza el format https://nomweb");
-                }
-            }
-
-
-
-            provadorRegex = String.valueOf(txtEditInstagram.getText());  //https://www.instagram.com/username
-            if (!provadorRegex.matches("^{2,25}[a-zA-Z._-]+\\.?")){
-                if(!provadorRegex.isEmpty()){
-                    validadorRegex = false;
-                    txtEditInstagram.setError("Instagram incorrecte, has d'incloure només el nom d'usuari (lletres i guions/barres baixes");
-                }
-            }
-
-            provadorRegex = String.valueOf(txtEditTwitter.getText()); // http://twitter.com/username
-            if (!provadorRegex.matches("^{2,25}[a-zA-Z._-]+\\.?")){
-                if(!provadorRegex.isEmpty()){
-                    validadorRegex = false;
-                    txtEditTwitter.setError("Twitter incorrecte, has d'incloure només el nom (lletres,guions i barres baixes");
-                }
-            }
-
-            provadorRegex = String.valueOf(txtEditFacebook.getText());  //http://www.facebook.com/someusername
-            if (!provadorRegex.matches("^{2,30}(.|\\s)*[a-zA-Z]+(.|\\s)*$")){
-                if(!provadorRegex.isEmpty()){
-                    validadorRegex = false;
-                    txtEditFacebook.setError("Facebook incorrecte, has d'incloure el nom d'usuari incluint nomes lletres");                }
-            }
-
-
-
-            if(validadorRegex){
-                Business business = new Business();
-                business.setNom(txtEditNom.getText().toString());
-                business.setDefinicio(txtEditDefinicio.getText().toString());
-                business.setTipus(agafarTipusNegoci());
-
-                String facebook = "";
-                if (!txtEditFacebook.getText().toString().isEmpty()) {
-                    facebook = "https://www.facebook.com/" + txtEditFacebook.getText().toString();
-                }
-                business.setFacebook(facebook);
-
-                String instagram = "";
-                if (!txtEditInstagram.getText().toString().isEmpty()) {
-                    instagram = "https://www.instagram.com/" + txtEditInstagram.getText().toString();
-                }
-                business.setInstagram(instagram);
-
-                String twitter = "";
-                if (!txtEditTwitter.getText().toString().isEmpty()) {
-                    twitter = "https://www.twitter.com/" + txtEditTwitter.getText().toString();
-                }
-                business.setTwitter(twitter);
-
-                String web = "";
-                if (!txtEditWeb.getText().toString().isEmpty()) {
-                    web = txtEditWeb.getText().toString();
-                }
-                business.setWeb(web);
-
-                createBusinessViewModel.createBusiness(business);
-
-                createBusinessViewModel.createBusinessResponse().observe(this, business1 -> {
-                    if (business1.isValid()) {
-                        Toast.makeText(getApplicationContext(), "Negoci registrat correctament", Toast.LENGTH_SHORT).show();
-                        startActivity (new Intent(CreateBusinessActivity.this, MainActivity.class));
-                    }
-                });
+            if(validate()){
+                register();
             }
         });
+
+        createBusinessViewModel.createBusinessResponse().observe(this, business1 -> {
+            if (business1.isValid()) {
+                // TODO : implementar foto
+                Toast.makeText(getApplicationContext(), "Negoci registrat correctament", Toast.LENGTH_SHORT).show();
+                startActivity (new Intent(CreateBusinessActivity.this, MainActivity.class));
+            }
+        });
+    }
+
+    private boolean validate () {
+
+        boolean validadorRegex = true;
+
+        String provadorRegex = String.valueOf(txtEditNom.getText());
+        if (!provadorRegex.matches("^{2,20}(.|\\s)*[a-zA-Z]+(.|\\s)*$")){
+
+            txtEditNom.setError("Nom no vàlid, ha d'incoure lletres nomes i un tamany de màxim 25 caràcters i mínim 2");
+            validadorRegex = false;
+
+        }
+
+        provadorRegex = String.valueOf(txtEditDefinicio.getText()); //qualsevol lletra, espais i enters permesos.
+        if (!provadorRegex.matches("^(.|\\s)*[a-zA-Z]+(.|\\s)*$")){
+
+            txtEditDefinicio.setError("Definició no valida. Ha d'incloure caracters");
+            validadorRegex = false;
+
+        }
+
+        provadorRegex = String.valueOf(txtEditWeb.getText()); //Inici amb http o https
+        if (!provadorRegex.matches("^https?:\\/\\/(.*)")){
+            if(!provadorRegex.isEmpty()){
+                validadorRegex = false;
+                txtEditWeb.setError("Si no teniu web deixeu buit el camp, si en teniu, utilitza el format https://nomweb");
+            }
+        }
+
+        provadorRegex = String.valueOf(txtEditInstagram.getText());  //https://www.instagram.com/username
+        if (!provadorRegex.matches("^{2,25}[a-zA-Z._-]+\\.?")){
+            if(!provadorRegex.isEmpty()){
+                validadorRegex = false;
+                txtEditInstagram.setError("Instagram incorrecte, has d'incloure només el nom d'usuari (lletres i guions/barres baixes");
+            }
+        }
+
+        provadorRegex = String.valueOf(txtEditTwitter.getText()); // http://twitter.com/username
+        if (!provadorRegex.matches("^{2,25}[a-zA-Z._-]+\\.?")){
+            if(!provadorRegex.isEmpty()){
+                validadorRegex = false;
+                txtEditTwitter.setError("Twitter incorrecte, has d'incloure només el nom (lletres,guions i barres baixes");
+            }
+        }
+
+        provadorRegex = String.valueOf(txtEditFacebook.getText());  //http://www.facebook.com/someusername
+        if (!provadorRegex.matches("^{2,30}(.|\\s)*[a-zA-Z]+(.|\\s)*$")){
+            if(!provadorRegex.isEmpty()){
+                validadorRegex = false;
+                txtEditFacebook.setError("Facebook incorrecte, has d'incloure el nom d'usuari incluint nomes lletres");                }
+        }
+
+        if (photo == null) {
+            Toast.makeText(getApplicationContext(), "Has de seleccionar la foto del negoci.", Toast.LENGTH_SHORT).show();
+            validadorRegex = false;
+        }
+
+        return validadorRegex;
+    }
+
+    private void register () {
+        Business business = new Business();
+        business.setNom(txtEditNom.getText().toString());
+        business.setDefinicio(txtEditDefinicio.getText().toString());
+        business.setTipus(agafarTipusNegoci());
+
+        String facebook = "";
+        if (!txtEditFacebook.getText().toString().isEmpty()) {
+            facebook = "https://www.facebook.com/" + txtEditFacebook.getText().toString();
+        }
+        business.setFacebook(facebook);
+
+        String instagram = "";
+        if (!txtEditInstagram.getText().toString().isEmpty()) {
+            instagram = "https://www.instagram.com/" + txtEditInstagram.getText().toString();
+        }
+        business.setInstagram(instagram);
+
+        String twitter = "";
+        if (!txtEditTwitter.getText().toString().isEmpty()) {
+            twitter = "https://www.twitter.com/" + txtEditTwitter.getText().toString();
+        }
+        business.setTwitter(twitter);
+
+        String web = "";
+        if (!txtEditWeb.getText().toString().isEmpty()) {
+            web = txtEditWeb.getText().toString();
+        }
+        business.setWeb(web);
+
+        createBusinessViewModel.createBusiness(business);
     }
 
     private String agafarTipusNegoci(){
@@ -165,12 +187,12 @@ public class CreateBusinessActivity extends AppCompatActivity {
         return text;
     }
 
-    /*public void checkExternalStoragePermission(){
+    public void checkExternalStoragePermission(){
         permissionManager.checkPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE, new PermissionManager.PermissionAskListener(){
 
             @Override
             public void onNeedPermission() {
-                ActivityCompat.requestPermissions(ConfigurationActivity.this,
+                ActivityCompat.requestPermissions(CreateBusinessActivity.this,
                         new String[]{ Manifest.permission.READ_EXTERNAL_STORAGE},
                         REQUEST_EXTERNAL_STORAGE);
             }
@@ -179,7 +201,7 @@ public class CreateBusinessActivity extends AppCompatActivity {
             public void onPermissionPreviouslyDenied() {
                 Log.d(TAG,"Permission denied");
 
-                ActivityCompat.requestPermissions(ConfigurationActivity.this,
+                ActivityCompat.requestPermissions(CreateBusinessActivity.this,
                         new String[]{ Manifest.permission.READ_EXTERNAL_STORAGE},
                         REQUEST_EXTERNAL_STORAGE);
             }
@@ -213,8 +235,8 @@ public class CreateBusinessActivity extends AppCompatActivity {
         if (resultCode == RESULT_OK) {
             Uri path = data.getData();
             File image = new File(getRealPathFromURI(path, this));
-            profileImage.setImageURI(path);
-            configurationViewModel.uploadAccountImage(image);
+            photopreview.setImageURI(path);
+            photo = image;
         }
     }
 
@@ -232,5 +254,5 @@ public class CreateBusinessActivity extends AppCompatActivity {
             return cursor.getString(column_index);
         }
         return uri.getPath();
-    }*/
+    }
 }
