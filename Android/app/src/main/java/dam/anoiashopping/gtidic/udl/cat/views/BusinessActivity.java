6f8a6943875f4d8f4e6a613ec97e345ef35e035c
abeelef <abelestebanferrer4@gmail.com>
2021-06-02 12:38:53 +0200

    parceleable bunlde funciona, falta afegir la recycledView

--- Android/app/src/main/java/dam/anoiashopping/gtidic/udl/cat/views/BusinessActivity.java
+++ Android/app/src/main/java/dam/anoiashopping/gtidic/udl/cat/views/BusinessActivity.java
@@ -3,22 +3,38 @@ package dam.anoiashopping.gtidic.udl.cat.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
 
 import dam.anoiashopping.gtidic.udl.cat.R;
 import dam.anoiashopping.gtidic.udl.cat.models.Business;
 
 public class BusinessActivity extends AppCompatActivity {
-
+    private ImageView imageBusiness;
+    private TextView txtNomBusiness;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_business);
+        imageBusiness = findViewById(R.id.im_botiga);
+        txtNomBusiness = findViewById(R.id.nom_empresa1);
+
 
         Business business = getIntent().getExtras().getParcelable("business");
 
-        String nom = business.getNom();
-        Toast.makeText(getApplicationContext(), nom, Toast.LENGTH_SHORT).show();
+        txtNomBusiness.setText(business.getNom());
+
+        if (business.getPhotoURL() != null) {
+            Picasso.get().load(business.getPhotoURL()).into(this.imageBusiness);
+        } else {
+            imageBusiness.setImageResource(R.drawable.user500);
+        }
+
 
     }
 
