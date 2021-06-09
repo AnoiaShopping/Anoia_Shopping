d6a654f8f5d31cd5f7f6d225f7cabc5cdb8d647b
Joan Francesc <Pedro Garcia>
2021-06-09 22:30:34 +0200

    Fragments preparats correctament

--- Android/app/src/main/java/dam/anoiashopping/gtidic/udl/cat/views/Activities/MenuActivity.java
+++ Android/app/src/main/java/dam/anoiashopping/gtidic/udl/cat/views/Activities/MenuActivity.java
@@ -1,16 +1,49 @@
 package dam.anoiashopping.gtidic.udl.cat.views.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.navigation.NavigationView;
 
 import dam.anoiashopping.gtidic.udl.cat.R;
 
 public class MenuActivity extends AppCompatActivity {
 
+    private AppBarConfiguration mAppBarConfiguration;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_chats,   R.id.nav_business,
+                R.id.nav_cart, R.id.nav_profile, R.id.nav_logout)
+                .setDrawerLayout(drawer)
+                .build();
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
